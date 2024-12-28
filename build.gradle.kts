18212256e0c040c57fd4910aa3260dbd9cf2a336
Kauma Mbewe <kaynerd26@gmail.com>
2024-12-28 17:45:49 +0200

    update build.gradle.kts to include Google services and Hilt dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build.gradle.kts
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
